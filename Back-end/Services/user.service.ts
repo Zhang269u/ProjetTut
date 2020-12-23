57a6589f054a7b55afb23869122426c8a982f392
1ner <49456933+1ner@users.noreply.github.com>
2020-12-23 20:02:02 +0100

    Added auth directory with its components and defined its service

--- Back-end/Services/user.service.ts
+++ Back-end/Services/user.service.ts
@@ -7,7 +7,7 @@ import { User } from '../Entities/user.entity';
 export class UsersService {
     constructor(
         @InjectRepository(User)
-        private usersRepository: Repository<User>,
+        private readonly usersRepository: Repository<User>,
     ) {}
 
     findAll(): Promise<User[]> {
