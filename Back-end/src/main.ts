cd34cb7f8527c5c61d659980dd390c62e2e06c8c
1ner <49456933+1ner@users.noreply.github.com>
2020-12-27 16:26:02 +0100

    Added logout, token manipulation and sessions.

--- Back-end/src/main.ts
+++ Back-end/src/main.ts
@@ -1,9 +1,10 @@
-
 import { NestFactory } from '@nestjs/core';
-import { DatabaseModule } from './database.module';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser'; //pour pouvoir lire les cookies
 
 async function bootstrap() {
-    const app = await NestFactory.create(DatabaseModule);
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
     await app.listen(3000);
 }
 bootstrap();
