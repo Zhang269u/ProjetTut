02d88e0b9a5060b31b7e0ad69e2241e2208c29ad
Alexycha <alexychalopin@gmail.com>
2021-01-12 17:59:18 +0100

    offre client

---
+++ src/app/model/signInData.ts
@@ -0,0 +1,16 @@
+export class SignInData{
+    private email: string;
+    private psw: string;
+
+    constructor(email:string, psw:string){
+        this.email = email;
+        this.psw = psw;
+    }
+
+    getEmail():string{
+    return this.email;
+    }
+    getPsw():string{
+        return this.psw;
+    }
+}
---
+++ src/app/service/authentification/authentification.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { SignInData } from 'src/app/model/signInData';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthentificationService {
+
+  private readonly mockedUser = new SignInData('alexy@gmail.com', 'test123');
+  isAuthentificated = false;
+
+  constructor(private router: Router) { }
+  
+  authentification(signInData : SignInData): boolean{
+if(this.checkCredentials(signInData)){
+  this.isAuthentificated = true;
+  this.router.navigate(['home']);
+  return true;
+}
+this.isAuthentificated = false;
+return false;
+
+ }
+private checkCredentials(signInData: SignInData): boolean{
+return this.checkEmail(signInData.getEmail()) && this.checkPsw(signInData.getPsw());
+}
+private checkEmail(email:string): boolean{
+  return email === this.mockedUser.getEmail();
+}
+private checkPsw(psw:string): boolean{
+return psw === this.mockedUser.getPsw();
+}
+}
