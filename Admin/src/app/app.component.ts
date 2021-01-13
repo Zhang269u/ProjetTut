3d3cbce2b694ef426ab2a7fb5fa4b1cf1bce3316
Lincker2u <70947143+Lincker2u@users.noreply.github.com>
2021-01-14 00:14:45 +0100

    boutons
    a faire: donner un noms aux boutons et afficher les blocks en fonction des boutoons

--- Admin/src/app/app.component.ts
+++ Admin/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Admin';
-}
+
+  isDisplay = false;
+  toggleDisplay(){
+    this.isDisplay = !this.isDisplay;
+    }
+  
+  }
